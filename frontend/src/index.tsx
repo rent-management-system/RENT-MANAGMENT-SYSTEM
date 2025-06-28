71353a1a42493b9c28375667cfc54684028c1907
dagiteferi <dagiteferi2011@gmail.com>
2025-06-28 22:21:10 +0300

    refactor: update import order in index and main files; remove unused tailwind.config.js

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './main.css';
+import './index.css';
 import App from './App.js';
 
 createRoot(document.getElementById('root')!).render(
