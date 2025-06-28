bbe536e8a547cbed72466bdd18d6c4774128fd25
dagiteferi <dagiteferi2011@gmail.com>
2025-06-28 22:03:39 +0300

    refactor: remove unused Login and Register components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Sonner as Toaster } from './components/ui/sonner.js';
-import Login from './components/auth/Login.js';
+// import Login from './components/auth/Login.js';
 import Register from './components/auth/Register.js';
 import GoogleCallback from './components/auth/GoogleCallback.js';
 import Dashboard from './pages/Dashboard.js';
@@ -11,7 +11,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/register" element={<Register />} />
         <Route path="/google-callback" element={<GoogleCallback />} />
         <Route path="/dashboard" element={<Dashboard />} />
