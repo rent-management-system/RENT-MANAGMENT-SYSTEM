2685132ffceb90a4bd1f7c3b5d5e45fa82d6a263
dagi <dagiteferi2011@gmail.com>
2025-11-11 01:45:28 +0300

    update token living

--- backend/user_service/app/core/config.py
+++ backend/user_service/app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     # JWT Settings
     JWT_SECRET: str
     JWT_ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     # Default Admin
